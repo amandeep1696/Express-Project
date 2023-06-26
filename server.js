789c103227b8f56ac66f2da118dfb1aae0cbead0
Amandeep Singh <am418470@dal.ca>
2023-06-26 19:02:05 -0300

    Express routers

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-const friendsController = require('./controllers/friends.controller');
-const messagesController = require('./controllers/messages.controller');
+const friendsRouter = require('./routes/friends.router');
+
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
@@ -12,17 +13,14 @@ app.use((req, res, next) => {
 
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta} ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends', friendsController.postFriend);
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
+app.use('/friends', friendsRouter);
 
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
