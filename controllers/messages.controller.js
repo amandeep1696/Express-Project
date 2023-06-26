0b68b30ed1f6d86f0508f816fc9934c8f4e15228
Amandeep Singh <am418470@dal.ca>
2023-06-26 19:26:25 -0300

    serving websites with node

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 function getMessages(req, res) {
   // res.send('<ul><li>Hello Albert</li></ul>');
-  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.webp'));
+  res.sendFile(
+    path.join(__dirname, '..', 'public', 'images', 'skimountain.webp')
+  );
 }
 
 function postMessage(req, res) {
