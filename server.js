ebfe0d7c483fe10faae79c4e315d6c11cfdd40d1
Amandeep Singh <am418470@dal.ca>
2023-06-26 17:06:54 -0300

    Some more express

--- server.js
+++ server.js
@@ -4,11 +4,31 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
+const friends = [
+  {
+    id: 0,
+    name: 'Albrt Einstein',
+  },
+  {
     id: 1,
     name: 'Isaac Newton',
-  });
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: 'friend does not exist',
+    });
+  }
 });
 
 app.get('/messages', (req, res) => {
